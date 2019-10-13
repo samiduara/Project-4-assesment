39d25560bb970d8b87169b59bdd631c921197733
sam <samgathon@hotmail.com>
2019-10-13 12:06:00 -0500

    Updates

---
+++ precache-manifest.54dbd7f30b3c695d412d840cb6d9b60c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99a8dddd89ad290f22f071df8659657e",
+    "url": "/samiduara/Project-4-assesment/index.html"
+  },
+  {
+    "revision": "d070ee8f54c4f1eb272d",
+    "url": "/samiduara/Project-4-assesment/static/css/main.7ada43a3.chunk.css"
+  },
+  {
+    "revision": "19d4402d8d9427a7201c",
+    "url": "/samiduara/Project-4-assesment/static/js/2.6446d4ee.chunk.js"
+  },
+  {
+    "revision": "d070ee8f54c4f1eb272d",
+    "url": "/samiduara/Project-4-assesment/static/js/main.80412458.chunk.js"
+  },
+  {
+    "revision": "1f6ad037137fdff310bc",
+    "url": "/samiduara/Project-4-assesment/static/js/runtime-main.5735614b.js"
+  }
+]);
